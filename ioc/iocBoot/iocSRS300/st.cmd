507f3158621c33e5ca32d25362dbaaad66394f8e
Georgiab61 <219304147+Georgiab61@users.noreply.github.com>
2025-12-05 14:46:24 +0000

    partial fix to 'busy' module error

--- ioc/iocBoot/iocSRS300/st.cmd
+++ ioc/iocBoot/iocSRS300/st.cmd
@@ -41,12 +41,12 @@ set_requestfile_path("$(TOP)/autoSaveRestore")
 # specify where request files can be found
 set_requestfile_path("$(AUTOSAVE)/asApp/Db/")
 
-dbLoadRecords("$(AUTOSAVE)/db/configMenu.db","P=$(PREFIX),R=$(SUFFIX),CONFIG=ramp1")
+dbLoadRecords("$(AUTOSAVE)/db/configMenu.db","P=$(PREFIX)$(SUFFIX),CONFIG=ramp1")
 
-set_pass0_restoreFile("RampSettings.sav", "P=$(PREFIX),R=$(SUFFIX),CONFIG=ramp1,CONFIGMENU=1")
-set_pass1_restoreFile("RampSettings.sav", "P=$(PREFIX),R=$(SUFFIX),CONFIG=ramp1,CONFIGMENU=1")
+set_pass0_restoreFile("ramp1Menu.sav", "P=$(PREFIX)$(SUFFIX),CONFIG=ramp1,CONFIGMENU=1")
+set_pass1_restoreFile("ramp1Menu.sav", "P=$(PREFIX)$(SUFFIX),CONFIG=ramp1,CONFIGMENU=1")
 
-create_monitor_set("ramp1Menu.req", 5 , "P=$(Prefix),CONFIG=ramp1,CONFIGMENU=1")
+create_monitor_set("ramp1Menu.req", 5 , "P=$(PREFIX)$(SUFFIX), CONFIG=ramp1,CONFIGMENU=1")
 #dbLoadRecords("$(TOP)/db/devSRS_PS375.db","P=$(PREFIX),R=$(SUFFIX),PORT=$(PORT),A=$(A)")
 
 #dbLoadRecords("db/devSRS_PS310_neg.db","P=$(P),R=$(SUFFIX),L=0,A=$(A)")
