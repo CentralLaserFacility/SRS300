f636d5dca8b60520c309b47e2e8810275d1c610e
Georgiab61 <219304147+Georgiab61@users.noreply.github.com>
2026-01-06 15:57:22 +0000

    prefix edits

--- ioc/iocBoot/iocSRS300/st.cmd
+++ ioc/iocBoot/iocSRS300/st.cmd
@@ -27,7 +27,7 @@ SRS300_registerRecordDeviceDriver pdbbase
 drvAsynIPPortConfigure("L0","$(ADDR)",0,0,0)
 
 ## Load record instances
-dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(DEVICE), R=asyn,PORT=$(PORT),ADDR=0,OMAX=256,IMAX=256")
+dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(DEVICE):, R=asyn,PORT=$(PORT),ADDR=0,OMAX=256,IMAX=256")
 dbLoadRecords("db/devSRS_PS3xx.db","PORT=$(PORT),DEVICE=$(DEVICE), A=$(A), MODEL=$(MODEL)")
 dbLoadTemplate("db/devSRS_PS3xx.substitutions","PORT=$(PORT),DEVICE=$(DEVICE), A=$(A), MODEL=$(MODEL)")
 dbLoadRecords("db/devSRS_PS3xx_ui.db","PORT=$(PORT),DEVICE=$(DEVICE), A=$(A), MODEL=$(MODEL)")
